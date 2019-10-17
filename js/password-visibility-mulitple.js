16042682e63f7e29728d6102bb9c33e60daaa9c8
sev the dev <sevwelker@gmail.com>
2019-10-17 08:05:34 -0700

    adds comments for clarity

--- js/password-visibility-mulitple.js
+++ js/password-visibility-mulitple.js
@@ -1,11 +1,10 @@
 //turn nodelist into an array
 var passwords = Array.prototype.slice.call(document.querySelectorAll('.password'));
-console.log(passwords);
 //grabs checkbox
 var togglePasswords = document.querySelector('#show-passwords');
-console.log(togglePasswords);
 
-//
+
+//allow user to toggle mulitple password fields at once
 togglePasswords.addEventListener('click', function(event) {
     if (togglePasswords.checked) {
         passwords.forEach(function (password, index) {
